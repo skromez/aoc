c8cd7d9faedf8098b363f4e30478b139ddda8802
Dmitrii Novikov <kromez12@gmail.com>
2024-12-10 10:45:42 +0100

    kekkkkkk

--- 2024/10.py
+++ 2024/10.py
@@ -60,21 +60,19 @@ R = len(grid)
 C = len(grid[0])
 
 trailheads = [(r, c) for r in range(R) for c in range(C) if grid[r][c] == 0]
+
 def score(grid, r, c):
     q = deque([(r, c)])
-    seen = {(r,c): 1}
     trails = 0
-    while len(q) > 0:
+    while q:
         cr, cc = q.popleft()
-        if grid[cr][cc] == 9:
-            trails += seen[(cr, cc)]
-        for nr, nc in [(cr - 1, cc), (cr, cc + 1), (cr + 1, cc), (cr, cc - 1)]:
-            if nr < 0 or nc < 0 or nr >= R or nc >= C: continue
-            if grid[nr][nc] != grid[cr][cc] + 1: continue
-            if (nr, nc) in seen: 
-                seen[(nr, nc)] += seen[(cr, cc)]
-                continue
-            seen[(nr, nc)] = seen[(cr, cc)]
-            q.append((nr, nc))
+        for nr, nc, in [(cr, cc + 1), (cr, cc - 1), (cr + 1, cc), (cr - 1, cc)]:
+            if 0 <= nr < R and 0 <= nc < C:
+                if grid[nr][nc] != grid[cr][cc] + 1: continue
+                if grid[nr][nc] == 9:
+                    trails += 1
+                else:
+                    q.append((nr, nc))
     return trails
+
 print(sum(score(grid, r,c) for r, c in trailheads))
